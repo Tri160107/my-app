1ef5598642fb17d9f1e926937e229c7ab7cc70e3
Tri160107 <trihuynh160107@gmail.com>
2024-06-28 14:39:33 +0700

    update 2

---
+++ app/index_buoi10.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index_buoi10 = () => {
+  return (
+    <View>
+      <Text>index_buoi10</Text>
+    </View>
+  )
+}
+
+export default index_buoi10
+
+const styles = StyleSheet.create({})
